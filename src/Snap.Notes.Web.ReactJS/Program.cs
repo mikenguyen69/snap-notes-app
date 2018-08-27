954ea4b86f13a8273e72fede8e570c45c851df2c
Mike Nguyen <nguyenduchung0609@gmail.com>
2018-08-28 06:43:10 +0800

    add new web
    remove razor web and add reactjs + redux for new web client to just comsume API

--- src/Snap.Notes.Web.ReactJS/Program.cs
+++ src/Snap.Notes.Web.ReactJS/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Snap.Notes.Web
+namespace Snap.Notes.Web.ReactJS
 {
     public class Program
     {
